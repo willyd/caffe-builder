dbf5c0ced7d3e1d3625fa9ad1d09c2a8f8e40fbe
Guillaume Dumont <dumont.guillaume@gmail.com>
2015-08-06 09:23:52 -0400

    Added support for building with OpenCV 3

--- cmake/packages/OpenCV.cmake
+++ cmake/packages/OpenCV.cmake
@@ -2,9 +2,17 @@ include(buildem_status)
 include(buildem_download_package)
 include(buildem_cmake_recipe)
 
-buildem_download_package(URL "http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.11/opencv-2.4.11.zip"
-						 SOURCE_DIR OpenCV_SOURCE_DIR
-                         )
+option(BUILD_OPENCV_3 "Build OpenCV 3.x instead of 2.4.x" ON)
+if(BUILD_OPENCV_3)
+    buildem_download_package(URL "http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/3.0.0/opencv-3.0.0.zip"
+						     SOURCE_DIR OpenCV_SOURCE_DIR
+                             )
+else()
+    buildem_download_package(URL "http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.11/opencv-2.4.11.zip"
+						     SOURCE_DIR OpenCV_SOURCE_DIR
+                             )
+endif()
+
 
 set(OpenCV_CMAKE_ARGS 
 	-DBUILD_SHARED_LIBS=OFF
@@ -14,11 +22,18 @@ set(OpenCV_CMAKE_ARGS
     -DBUILD_PACKAGE=OFF
     -DBUILD_WITH_STATIC_CRT=OFF
     -DBUILD_opencv_apps=OFF
-    -DBUILD_opencv_gpu=OFF
+
     -DBUILD_opencv_python=OFF
+    -DBUILD_opencv_python2=OFF
+    -DBUILD_opencv_python3=OFF
     -DBUILD_opencv_ts=OFF
     -DBUILD_ZLIB=OFF
+    # enable these if you want GPU support in OpenCV
     -DWITH_CUDA=OFF
+    -DBUILD_opencv_gpu=OFF
+    # change according to your GPU
+    -DCUDA_ARCH_BIN=5.2
+    -DCUDA_ARCH_PTX=""
     )
     
 buildem_cmake_recipe(NAME OpenCV
