a04d12af4cdc544f624aedd781882a523391cda2
Guillaume Dumont <dumont.guillaume@gmail.com>
2016-09-26 09:19:07 -0400

    WIP on making absolute paths relative

--- fix_library_paths.py
+++ fix_library_paths.py
@@ -1,4 +1,36 @@
-import os
 
-def fix_lib_path(cmake_file)
-    cmake_path = os.path.abspath(cmake_file)
+# coding: utf-8
+
+# In[15]:
+
+import os
+import re
+pattern = r'([a-zA-Z]:/(((?![<>:"/|?*]).)+((?<![ .])/)?)*)'
+regex = re.compile(pattern)
+filename = r"D:\Work\lib\caffe\build\install\cmake\hdf5-targets.cmake"
+with open(filename, 'r') as f:
+    content = f.read()
+
+
+# In[22]:
+
+basepath = os.path.abspath(os.path.dirname(filename))
+basepath
+prefix = basepath.replace('\\', '/')
+
+
+# In[25]:
+
+matches = regex.findall(content)
+for m in matches:
+    path = os.path.join(basepath, os.path.relpath(m[0], basepath))
+    path = path.replace('\\', '/')
+    path = path.replace(prefix, '${CMAKE_CURRENT_LIST_DIR}')
+    content = content.replace(m[0], path)
+print(content)
+
+
+# In[ ]:
+
+
+
