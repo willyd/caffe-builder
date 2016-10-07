2b0af328dfcf95037bcb91a3aa1561ab5a27ee96
Guillaume Dumont <dumont.guillaume@gmail.com>
2016-10-06 20:11:37 -0400

    Removed unused files
    Added .vscode folder to gitignore

--- cmake/modules/FindProtobuf.cmake
+++
@@ -1,4 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/GenericFindModuleConfig.cmake)
-set(PROTOBUF_LIBRARIES libprotobuf)
-set(PROTOBUF_LITE_LIBRARIES libprotobuf-lite)
-set(PROTOBUF_PROTOC_LIBRARIES libprotoc)
