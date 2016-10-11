937c5e182937e99648e0dcab90a604b0a94acbf6
Guillaume Dumont <dumont.guillaume@gmail.com>
2016-10-10 21:43:19 -0400

    Added recipe for protobuf conda package

---
+++ packages/protobuf/recipe/bld.bat
@@ -0,0 +1,24 @@
+mkdir build
+pushd build
+
+:: Configure
+cmake -GNinja ^
+      -D CB_BUILD_CONDA_PKG=ON ^
+      -D CB_BUILD_ALL=OFF ^
+      -D BUILD_PROTOBUF=ON ^
+      -D PROTOBUF_WITH_PYTHON=ON ^
+      -D PROTOBUF_BUILD_SHARED_LIBS=OFF ^
+      -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+      %SRC_DIR%
+
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build .
+if errorlevel 1 exit 1
+
+popd
+
+:: Remove confusing files
+rm %LIBRARY_PREFIX%\caffe-builder-config.cmake
+rm %LIBRARY_PREFIX%\prependpath.bat
